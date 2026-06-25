37cd6ad4856e4e16d2b605f82e75c4b91af50c8a
Paulo Vinícius Toledo <pvvtoledo@gmail.com>
2026-06-25 17:08:42 -0300

    feat: metallic yield in example materials

--- backend/app/optimizer.py
+++ backend/app/optimizer.py
@@ -2,29 +2,29 @@ import pulp as p
 
 materials = [
 
-    {"name": "Pig Iron", "price": 2500, "fe": 94.56, "c": 3.8, "si": 1.0, "mn": 0.5, "p": 0.10, "s": 0.04,  "cu": 0.01, "ni": 0.03, "sio2": 0.00, "al2o3": 0.00, "cao": 0.00, "mgo": 0.00, "feo": 0.00 },
+    {"name": "Pig Iron", "price": 2500, "fe": 94.56, "c": 3.8, "si": 1.0, "mn": 0.5, "p": 0.10, "s": 0.04,  "cu": 0.01, "ni": 0.03, "sio2": 0.00, "al2o3": 0.00, "cao": 0.00, "mgo": 0.00, "feo": 0.00, "metallic_yield": 0.94 },
 
-    {"name": "HBI (hot briquetted iron)", "price": 2000, "fe": 89.45, "c": 2.10, "si": 0.12, "mn": 0.08, "p": 0.04, "s": 0.01,  "cu": 0.01, "ni": 0.01, "sio2": 1.50, "al2o3": 0.40, "cao": 0.30, "mgo": 0.50, "feo": 5.50 },
+    {"name": "HBI (hot briquetted iron)", "price": 2000, "fe": 89.45, "c": 2.10, "si": 0.12, "mn": 0.08, "p": 0.04, "s": 0.01,  "cu": 0.01, "ni": 0.01, "sio2": 1.50, "al2o3": 0.40, "cao": 0.30, "mgo": 0.50, "feo": 5.50,"metallic_yield": 0.90 },
 
-    {"name": "Cast Iron", "price": 1500, "fe": 93.80, "c": 3.50, "si": 2.20, "mn": 0.45, "p": 0.07, "s": 0.05,  "cu": 0.15, "ni": 0.01, "sio2": 0.00, "al2o3": 0.00, "cao": 0.00, "mgo": 0.00, "feo": 0.00 },
+    {"name": "Cast Iron", "price": 1500, "fe": 93.80, "c": 3.50, "si": 2.20, "mn": 0.45, "p": 0.07, "s": 0.05,  "cu": 0.15, "ni": 0.01, "sio2": 0.00, "al2o3": 0.00, "cao": 0.00, "mgo": 0.00, "feo": 0.00, "metallic_yield": 0.88 },
 
-    {"name": "Obsolete Scrap", "price": 1000, "fe": 98.37, "c": 0.18, "si": 0.26, "mn": 0.55, "p": 0.05, "s": 0.10,  "cu": 0.30, "ni": 0.05, "sio2": 0.00, "al2o3": 0.00, "cao": 0.00, "mgo": 0.00, "feo": 0.18 },
+    {"name": "Obsolete Scrap", "price": 1000, "fe": 98.37, "c": 0.18, "si": 0.26, "mn": 0.55, "p": 0.05, "s": 0.10,  "cu": 0.30, "ni": 0.05, "sio2": 0.00, "al2o3": 0.00, "cao": 0.00, "mgo": 0.00, "feo": 0.18, "metallic_yield": 0.85 },
 
     {"name": "Turnings", "price": 1600, "fe": 97.25, "c": 0.40, "si": 0.20, "mn": 1.10, "p": 0.05, "s": 0.04,  "cu": 0.10, "ni": 0.37, "sio2": 0.00, "al2o3": 0.00, "cao": 0.00, "mgo": 0.00, "feo": 0.10 },
 
-    {"name": "Automotive Stamping Bundles", "price": 2200, "fe": 97.90, "c": 0.05, "si": 0.22, "mn": 0.43, "p": 0.02, "s": 0.01,  "cu": 0.10, "ni": 0.03, "sio2": 1.30, "al2o3": 0.00, "cao": 0.00, "mgo": 0.00, "feo": 0.10 }
+    {"name": "Automotive Stamping Bundles", "price": 2200, "fe": 97.90, "c": 0.05, "si": 0.22, "mn": 0.43, "p": 0.02, "s": 0.01,  "cu": 0.10, "ni": 0.03, "sio2": 1.30, "al2o3": 0.00, "cao": 0.00, "mgo": 0.00, "feo": 0.10, "metallic_yield": 0.92 }
 ]
 
 eaf_capacity = 120
 target_yield = 0.90
 
 c_min =  1.5
-si_min = 0.50
+si_min = 0.80
 mn_min = 0.60
-p_limit = 0.05
-s_limit = 0.04
-cu_limit = 0.30
-ni_limit = 0.03
+p_limit = 0.09
+s_limit = 0.09
+cu_limit = 0.70
+ni_limit = 0.80
 
 prob = p.LpProblem("eaf_optmizer", p.LpMinimize)
 
