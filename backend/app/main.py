918b2b0ff3dff3302f4ac1328a49317c4e262f3e
Paulo Vinícius Toledo <pvvtoledo@gmail.com>
2026-07-12 22:06:48 -0300

    feat: env for CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -5,9 +7,11 @@ from app.routers.optimize import optimization_router
 
 app = FastAPI()
 
+allowed_origins = os.environ.get("ALLOWED_ORIGINS", "http://localhost:3000").split(",")
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["http://localhost:3000"],
+    allow_origins=allowed_origins,
     allow_methods=["POST"],
     allow_headers=["Content-Type"],
 )
